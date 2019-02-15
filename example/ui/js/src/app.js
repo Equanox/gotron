2473f4b522649a694d4ebd13856c8029c0f03dc5
Puengel <daniel.puengel@gmail.com>
2019-02-16 00:13:43 +0100

    Port now known at frontend

--- example/ui/js/src/app.js
+++ example/ui/js/src/app.js
@@ -1,11 +1,21 @@
 require("./style.css");
 
 window.onload = function () {
+  var container = document.createElement("div");
+
   var topic = document.createElement("h1");
   topic.className = 'topic';
   topic.innerHTML = 'Hello Gotron / JS';
 
-  document.body.appendChild(topic);
+  container.appendChild(topic);
+
+  //Show backend port
+  var port = document.createElement("p");
+  port.className = 'topic';
+  port.innerHTML = `Backend Port: ${global.backendPort}`;
+  container.appendChild(port);
+
+  document.body.appendChild(container);
 }
 
 //Reload on keypress 'r'
